6ed4a38cea8ea07013bb59484194c60861d565e8
notarioale <alealenotario@gmail.com>
2022-08-21 23:28:52 -0400

    First test

---
+++ tests/test_demo.py
@@ -0,0 +1,13 @@
+import pytest
+from django.urls import reverse
+
+
+# Test the Pytest library
+def test_working():
+   pass
+
+
+# Test the admin view
+def test_admin_view(admin_client):
+    response = admin_client.get('/admin/')
+    assert response.status_code == 200
